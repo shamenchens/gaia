c968f3866e7fc57525f79e3a8b1d8a8d3a563ace
Luke Chang <lchang@mozilla.com>
2014-03-18 14:46:47 +0800

    * let system/homescreen_window.js use real launch_path in manifest

--- apps/system/js/homescreen_window.js
+++ apps/system/js/homescreen_window.js
@@ -74,7 +74,7 @@
       var app = Applications.getByManifestURL(manifestURL);
       this.origin = app.origin;
       this.manifestURL = app.manifestURL;
-      this.url = app.origin + '/index.html#root';
+      this.url = app.origin + app.manifest.launch_path;
 
       this.browser_config =
         new BrowserConfigHelper(this.origin, this.manifestURL);
@@ -171,7 +171,8 @@
     if (!this.element) {
       this.render();
     } else if (reset) {
-      this.browser.element.src = this.browser_config.url + new Date();
+      this.browser.element.src = this.browser_config.url +
+        (this.browser_config.url.indexOf('#') < 0 ? '#' : '?') + new Date();
     }
 
     return this.element;
