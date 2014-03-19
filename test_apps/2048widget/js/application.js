30dedf50eeccd816d000594eb56fcd7d51f9d533
Sherman Chen <shamen.chens@gmail.com>
2014-03-19 16:07:47 +0800

    create 2048 widget

---
+++ test_apps/2048widget/js/application.js
@@ -0,0 +1,8 @@
+restartDelay = 3000;
+animationDelay = 300;
+minSearchTime = 100;
+
+// Wait till the browser is ready to render the game (avoids glitches)
+window.requestAnimationFrame(function () {
+  var manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
+});
