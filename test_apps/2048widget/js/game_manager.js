c20d5d4b6ebd2519902a991a2ed5e7a0d4b842cb
Sherman Chen <shamen.chens@gmail.com>
2014-03-19 17:03:45 +0800

    [Widget] Create 2048 widget

---
+++ test_apps/2048widget/js/game_manager.js
@@ -0,0 +1,102 @@
+function GameManager(size, InputManager, Actuator) {
+  this.size         = size; // Size of the grid
+  this.inputManager = new InputManager;
+  this.actuator     = new Actuator;
+
+  this.running      = false;
+
+  this.inputManager.on("move", this.move.bind(this));
+  this.inputManager.on("restart", this.restart.bind(this));
+
+  this.inputManager.on('think', function() {
+    var best = this.ai.getBest();
+    this.actuator.showHint(best.move);
+  }.bind(this));
+
+  this.inputManager.on('run', function() {
+    if (this.running) {
+      this.running = false;
+    } else {
+      this.running = true;
+      this.run();
+    }
+  }.bind(this));
+
+  this.setup();
+}
+
+// Restart the game
+GameManager.prototype.restart = function () {
+  this.actuator.restart();
+  this.running = false;
+  this.setup();
+};
+
+// Set up the game
+GameManager.prototype.setup = function () {
+  this.grid         = new Grid(this.size);
+  this.grid.addStartTiles();
+
+  this.ai           = new AI(this.grid);
+
+  this.score        = 0;
+  this.over         = false;
+  this.won          = false;
+
+  // Update the actuator
+  this.actuate();
+  this.inputManager.emit('run');
+};
+
+
+// Sends the updated grid to the actuator
+GameManager.prototype.actuate = function () {
+  this.actuator.actuate(this.grid, {
+    score: this.score,
+    over:  this.over,
+    won:   this.won
+  });
+};
+
+// makes a given move and updates state
+GameManager.prototype.move = function(direction) {
+  var self = this;
+  var result = this.grid.move(direction);
+  var restartDelay = 3000;
+  this.score += result.score;
+
+  if (!result.won) {
+    if (result.moved) {
+      this.grid.computerMove();
+    }
+  } else {
+    this.won = true;
+    setTimeout(function(){
+      self.restart();
+    }, restartDelay);
+  }
+
+  //console.log(this.grid.valueSum());
+
+  if (!this.grid.movesAvailable()) {
+    this.over = true; // Game over!
+    setTimeout(function(){
+      self.restart();
+    }, restartDelay);
+  }
+
+  this.actuate();
+}
+
+// moves continuously until game is over
+GameManager.prototype.run = function() {
+  var best = this.ai.getBest();
+  this.move(best.move);
+  var animationDelay = 300;
+  if (this.running && !this.over && !this.won) {
+    var self = this;
+    setTimeout(function(){
+      self.run();
+    }, animationDelay);
+  }
+}
