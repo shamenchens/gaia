6c1cc60345d859975e45a84cc006871e3daa6e49
Tzu-Lin Huang <tzhuang@mozilla.com>
2014-03-20 18:51:55 +0800

    Keep unresponse request in array

--- apps/system/js/homescreen_connection.js
+++ apps/system/js/homescreen_connection.js
@@ -7,6 +7,26 @@
     window.addEventListener('iac-homescreen-request', this);
   };
 
+  // System app should expect incoming array of request objects like
+  // [{
+  //   requestId: ‘87548a9b-3e64-4aca-9366-f4856e5f7a65’,
+  //   action: ‘add’,
+  //   args: {
+  //     x: 220,
+  //     y: 10,
+  //     w: 200,
+  //     h: 150,
+  //     widgetOrigin: 'app://widget.gaiamobile.org/',
+  //     widgetEntryPoint: 'entry'
+  //   }
+  // }]
+  // and should response with something like this
+  // {
+  //   requestId: ‘87548a9b-3e64-4aca-9366-f4856e5f7a65’,
+  //   action: add,
+  //   result: true,
+  //   widgetId: ‘widget-idY’
+  // }
   HomescreenConnection.prototype = {
     start: function hc_start() {
       console.log('HomescreenConnection started!');
@@ -19,24 +39,68 @@
       var messageObject = evt.detail;
       console.log('received homescreen-request message:[' +
         JSON.stringify(messageObject) + ']');
+      // TODO: validate request action object
+      if (messageObject && messageObject.requestId) {
+        this._pendingRequests.push(messageObject);
+      }
       window.dispatchEvent(
-        new CustomEvent('homescreen-action-object', {'detail': messageObject}));
+        new CustomEvent('homescreen-action-object',
+          {'detail': [messageObject]}));
     },
-    _sendMessage: function hc_sendMessage(message) {
+    _pendingRequests: [],
+    _removePendingRequest: function sc_findUnresponsiveRequest(requestObject) {
+      var that = this;
+      try {
+        this._pendingRequests.forEach(function(value, index) {
+          if (value.requestId === requestObject.requestId) {
+            that._pendingRequests.splice(index, 1);
+            throw new Error('Break');
+          }
+        });
+      } catch (e) {
+        if (e.message !== 'Break') {
+          throw e;
+        }
+      }
+    },
+    _sendMessage: function hc_sendMessage(
+        message, successCallback, errorCallback) {
       navigator.mozApps.getSelf().onsuccess = function(evt) {
         var app = evt.target.result;
         app.connect('system-response').then(function onConnAccepted(ports) {
           ports.forEach(function(port) {
             port.postMessage(message);
           });
+          if (typeof successCallback === 'function') {
+            successCallback(message);
+          }
         }, function onConnRejected(reason) {
           console.log('connection rejected due to: ' + reason);
+          if (typeof errorCallback === 'function') {
+            errorCallback(reason, message);
+          }
         });
       };
+    },
+    _packResponseObject: function hc_packResponseObject(
+        requestId, action, result, widgetId) {
+      return {
+        requestId: requestId,
+        action: action,
+        result: result,
+        widgetId: widgetId
+      };
+    },
+    confirm: function hc_confirm(requestId, action, widgetId) {
+      this._sendMessage(
+        this._packResponseObject(requestId, action, true, widgetId),
+        this._removePendingRequest.bind(this));
+    },
+    deny: function hc_deny(requestId, action, widgetId) {
+      this._sendMessage(
+        this._packResponseObject(requestId, action, false, widgetId));
     }
   };
-
   exports.HomescreenConnection = HomescreenConnection;
 }(window));
-
 window.homescreenConnection = new HomescreenConnection().start();
--- test_apps/homescreen-tv-arch1/js/system_connection.js
+++ test_apps/homescreen-tv-arch1/js/system_connection.js
@@ -2,11 +2,31 @@
 
 /* globals SystemConnection */
 
-(function(exports){
+(function(exports) {
   var SystemConnection = function() {
     window.addEventListener('iac-system-response', this);
   };
 
+  // Homescreen app should send request object like this
+  // {
+  //   requestId: ‘87548a9b-3e64-4aca-9366-f4856e5f7a65’,
+  //   action: ‘add’,
+  //   args: {
+  //     x: 220,
+  //     y: 10,
+  //     w: 200,
+  //     h: 150,
+  //     widgetOrigin: 'app://widget.gaiamobile.org/',
+  //     widgetEntryPoint: 'entry'
+  //   }
+  // }
+  // and shoould expect incoming array of response objects like
+  // [{
+  //   requestId: ‘87548a9b-3e64-4aca-9366-f4856e5f7a65’,
+  //   action: 'add',
+  //   result: true,
+  //   widgetId: ‘widget-idY’
+  // }]
   SystemConnection.prototype = {
     start: function sc_start() {
       console.log('SystemConnection started!');
@@ -19,22 +39,52 @@
       var messageObject = evt.detail;
       console.log('received system-response message: [' +
         JSON.stringify(messageObject) + ']');
+      // TODO: validation
+      if (messageObject && messageObject.requestId) {
+        this._removeUnresponsiveRequest(messageObject.requestId);
+      }
       window.dispatchEvent(
-        new CustomEvent('system-action-object', {'detail': messageObject}));
+        new CustomEvent('system-action-object', {'detail': [messageObject]}));
     },
-    _sendMessage: function sc_sendMessage(message) {
+    _unresponsiveRequests: [],
+    _removeUnresponsiveRequest: function sc_findUnresponsiveRequest(requestId) {
+      var that = this;
+      try {
+        this._unresponsiveRequests.forEach(function(value, index) {
+          if (value.requestId === requestId) {
+            that._unresponsiveRequests.splice(index, 1);
+            throw new Error('Break');
+          }
+        });
+      } catch (e) {
+        if (e.message !== 'Break') {
+          throw e;
+        }
+      }
+    },
+    _sendMessage: function sc_sendMessage(
+        message, successCallback, errorCallback) {
       navigator.mozApps.getSelf().onsuccess = function(evt) {
         var app = evt.target.result;
         app.connect('homescreen-request').then(function onConnAccepted(ports) {
           ports.forEach(function(port) {
             port.postMessage(message);
           });
+          if (typeof successCallback === 'function') {
+            successCallback(message);
+          }
         }, function onConnRejected(reason) {
           console.log('connection rejected due to: ' + reason);
+          if (typeof errorCallback === 'function') {
+            errorCallback(reason, message);
+          }
         });
       };
     },
-    _packActionObject: function sc_packActionObject(action, args) {
+    _waitForResponse: function sc_waitForResponse(requestObject) {
+      this._unresponsiveRequests.push(requestObject);
+    },
+    _packRequestObject: function sc_packRequestObject(action, args) {
       return {
         requestId: uuid.v4(),
         action: action,
@@ -42,19 +92,22 @@
       };
     },
     addWidget: function sc_addWidget(args) {
-      var actionObject = this._packActionObject('add', args);
-      this._sendMessage(actionObject);
-      return actionObject.requestId;
+      var requestObject = this._packRequestObject('add', args);
+      // TODO: validation on args and refactoring
+      this._sendMessage(requestObject, this._waitForResponse.bind(this));
+      return requestObject.requestId;
     },
     removeWidget: function sc_removeWidget(args) {
-      var actionObject = this._packActionObject('remove', args);
-      this._sendMessage(actionObject);
-      return actionObject.requestId;
+      var requestObject = this._packRequestObject('remove', args);
+      // TODO: validation on args and refactoring
+      this._sendMessage(requestObject, this._waitForResponse.bind(this));
+      return requestObject.requestId;
     },
     updateWidget: function sc_updateWidget(args) {
-      var actionObject = this._packActionObject('update', args);
-      this._sendMessage(actionObject);
-      return actionObject.requestId;
+      var requestObject = this._packRequestObject('update', args);
+      // TODO: validation on args and refactoring
+      this._sendMessage(requestObject, this._waitForResponse.bind(this));
+      return requestObject.requestId;
     }
   };
   exports.SystemConnection = SystemConnection;
