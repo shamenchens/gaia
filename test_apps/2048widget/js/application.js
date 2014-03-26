44eee4158b1e585554385029f1248921f5e625d0
Sherman Chen <shamen.chens@gmail.com>
2014-03-26 19:19:51 +0800

    2048 widget update

--- test_apps/2048widget/js/application.js
+++ test_apps/2048widget/js/application.js
@@ -1,4 +1,9 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
   var manager = new GameManager(4, KeyboardInputManager, HTMLActuator);
+  var minPixel = Math.min(window.innerHeight, window.innerWidth);
+	var container = document.getElementById('2048container');
+	container.style.width = minPixel + 'px';
+	container.style.height = minPixel + 'px';
 });
+
