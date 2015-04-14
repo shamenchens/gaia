2ec1234ea772400070a6c804066f8ec1dbd85cf7
Sherman Chen <shamen.chens@gmail.com>
2015-04-14 17:24:46 +0800

    Add tv debug distribution folder to test on tv/phone system merge

---
+++ tv-debug/partner-prefs.js
@@ -0,0 +1,6 @@
+user_pref('b2g.system_startup_url',
+          'app://system.gaiamobile.org/index.html');
+user_pref('b2g.system_manifest_url',
+          'app://system.gaiamobile.org/manifest.webapp');
+user_pref('b2g.neterror.url',
+          'app://system.gaiamobile.org/net_error.html');
