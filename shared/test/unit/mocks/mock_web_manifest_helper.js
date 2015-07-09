151fbdc13d1e81522d85aabb54d89a2962b4ac0d
Alive Kuo <alegnadise@gmail.com>
2015-07-09 17:06:00 +0800

    Isolate mocks

--- shared/test/unit/mocks/mock_web_manifest_helper.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* exported MockWebManifestHelper */
-var MockWebManifestHelper = {
-  processRawManifest: function() {},
-  getManifest: function(manifestURL) {
-    return Promise.resolve({'short_name': 'App', 'name': 'My App'});
-  }
-};
